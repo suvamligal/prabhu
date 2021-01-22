5a4a949726d3a06d5d60066328dcc9ae9b6d301e
Suvam Ligal <suvamligal@gmail.com>
2021-01-21 19:27:46 -0600

    removed spring security

--- src/main/java/com/prabhubank/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.prabhubank.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-}
